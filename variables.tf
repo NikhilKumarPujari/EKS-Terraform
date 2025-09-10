d549ae221cc9bba1dbf053bce11f3f193df22889
Nikhil Kumar <144585007+NikhilKumarPujari@users.noreply.github.com>
2025-09-10 15:35:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "Master"  # this is .pem key name for instances
+  default     = "master"  # this is .pem key name for instances
 }
